ddcd23d1c49c33beb4e2937bf62e55310b8420fe
Andrew Kersten <akersten5@student.cvtc.edu>
2018-10-06 17:22:42 -0500

    Use new calendar generation code on schedule page.  Add an attribute to session <li> elements so Anderson can style them.

--- Front end/js/calendar.js
+++ Front end/js/calendar.js
@@ -1,81 +1,111 @@
-function buildCalendar(year, month) {
-	let dateFirst = new Date(year, month, 1);
-	let dateLast = new Date(year, month + 1, 0);
+function createCalendar(parentElement, year, month, onDateCreated) {
 
-	let monthShortName = dateFirst.toLocaleString("en-us", { month: "short" });
-	let monthLongName = dateFirst.toLocaleString("en-us", { month: "long" });
+    // Get the first and last date of the selected month
 
-	let html = `
-		<table>
-			<thead>
-				<tr>
-					<th colspan="7">${monthLongName} ${year}</th>
-				</tr>
-				<tr>
-					<th>Sunday</th>
-					<th>Monday</th>
-					<th>Tuesday</th>
-					<th>Wednesday</th>
-					<th>Thursday</th>
-					<th>Friday</th>
-					<th>Saturday</th>
-				</tr>
-			</thead>
-			<tbody>
-	`;
+    let dateFirst = new Date(year, month, 1);
+    let dateLast = new Date(year, month + 1, 0);
 
-	let dates = [];
+    // Generate the basic table elements
 
-	// Pad the table at the start of the month
-	for (let i = 0; i < dateFirst.getDay(); i++) {
-		dates.push("");
-	}
+    let table = document.createElement("table");
 
-	for (let i = 1; i <= dateLast.getDate(); i++) {
-		dates.push(i);
+    let thead = document.createElement("thead");
+    let tbody = document.createElement("tbody");
 
-		if (dates.length == 7) {
-			addRow(dates);
-			dates = [];
-		}
-	}
+    table.appendChild(thead);
+    table.appendChild(tbody);
 
-	// Pad the table at the end of the month
-	if (dates.length > 0) {
-		while (dates.length < 7) {
-			dates.push("");
-		}
-		addRow(dates);
-	}
+    // Generate the month thead row
 
-	html += '</tbody></table>'
+    let monthRow = document.createElement("tr");
+    let monthCell = document.createElement("th");
+    monthCell.setAttribute("colspan", "7");
+    monthCell.appendChild(document.createTextNode(`${dateFirst.toLocaleString("en-us", { month: "long" })} ${year}`));
+    monthRow.appendChild(monthCell);
 
-	document.write(html);
+    thead.appendChild(monthRow);
 
-	function addRow(dates) {
-		html += '<tr>';
+    // Generate the Sunday - Saturday thead row
 
-		for (let d in dates) {
-			addCell(dates[d]);
-		}
+    let days = document.createElement("tr");
 
-		html += '</tr>';
-	}
+    addDay(days, "Sunday");
+    addDay(days, "Monday");
+    addDay(days, "Tuesday");
+    addDay(days, "Wednesday");
+    addDay(days, "Thursday");
+    addDay(days, "Friday");
+    addDay(days, "Saturday");
 
-	function addCell(date) {
-		if (!date) {
-			html += '<td></td>';
-			return;
-		}
+    thead.appendChild(days);
 
-		let cellId = monthShortName + date;
+    // Helper function for the Sunday - Saturday thead row generation
 
-		html += `
-			<td>
-				<div>${date}</div>
-				<ul id=${cellId}>
-				</ul>
-			</td>
-		`;
-	}
+    function addDay(parent, day) {
+        let element = document.createElement("th");
+        let text = document.createTextNode(day);
+        element.appendChild(text);
+        parent.appendChild(element);
+    }
+
+    // TBODY
+
+    let dates = [];
+
+    // The first date of the month can start on any day of the week.  The table needs to
+    // be padded to properly align the dates with the correct day of the week.  By using
+    // negative dates it is easy to include the dates from the previous month the calendar.
+
+    for (let i = dateFirst.getDay(); i > 0; i--) {
+        dates.push(-1 * (i - 1));
+    }
+
+    // Churn through the dates of the month and add them to the dates array.  Once the dates
+    // array contains 7 dates add the week to the calendar and reset the dates array.
+
+    for (let i = 1; i <= dateLast.getDate(); i++) {
+        dates.push(i);
+        if (dates.length == 7) {
+            addWeek(tbody, dates);
+            dates = [];
+        }
+    }
+
+    // The last date of the month can fall on any day of the week.  The rest of the week is filled
+    // in with the dates from the coming month.
+
+    if (dates.length > 0) {
+        let i = dateLast.getDate();
+        while (dates.length < 7) {
+            dates.push(++i);
+        }
+        addWeek(tbody, dates);
+    }
+
+    // Helper function to add a week (row) to the calendar (table)
+
+    function addWeek(parent, dates) {
+        let row = document.createElement("tr");
+        for (let date of dates) {
+            addDate(row, date);
+        }
+        parent.appendChild(row);
+    }
+
+    // Helper function to add a date (cell) to the week (row)
+
+    function addDate(parent, date) {
+        let cell = document.createElement("td");
+        if (date <= 0 || date > dateLast.getDate()) {
+            cell.setAttribute("class", "inactive");
+        }
+        if (onDateCreated) {
+            onDateCreated(cell, new Date(year, month, date));
+        }
+        parent.appendChild(cell);
+    }
+
+    // Add the calendar to the document
+
+    parentElement.appendChild(table);
 }
